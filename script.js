0a446234238685255eb86f60eaf0c1297210a48b
Edmond GAKS <edmondgaks@gmail.com>
2022-03-06 19:26:44 +0200

    adding our modal

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i<btnsOpenModal.length; i++) 
+    console.log(btnsOpenModal[i].addEventListener('click', function () {
+        console.log('Button clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })); 
