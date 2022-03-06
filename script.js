c3c21a17fca713aeedbef2cd787b8820805afe83
Edmond GAKS <edmondgaks@gmail.com>
2022-03-06 19:52:45 +0200

    adding our modal

--- script.js
+++ script.js
@@ -12,4 +12,12 @@ for (let i = 0; i<btnsOpenModal.length; i++)
         console.log('Button clicked');
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
-    })); 
+
+
+        modal.style.display = 'block';
+    }));
+
+    btnCloseModal.addEventListener('click', function () {
+         modal.classList.add('hidden');
+         overlay.classList.add('hidden');
+    });
