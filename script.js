507f8dce90a9589242190bb1e3a1bf4ecc4c1245
Edmond GAKS <edmondgaks@gmail.com>
2022-03-08 17:13:49 +0200

    handling esc key event

--- script.js
+++ script.js
@@ -5,19 +5,30 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+
+const openModal = function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
 
 for (let i = 0; i<btnsOpenModal.length; i++) 
-    console.log(btnsOpenModal[i].addEventListener('click', function () {
-        console.log('Button clicked');
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
+    btnsOpenModal[i].addEventListener('click', openModal);
 
 
-        modal.style.display = 'block';
-    }));
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-    btnCloseModal.addEventListener('click', function () {
-         modal.classList.add('hidden');
-         overlay.classList.add('hidden');
-    });
+document.addEventListener('keydown', function(e) {
+    console.log('A key was pressed');
+    console.log(e.key);
+
+    if(e.key === 'Escape')
+    console.log('Esc was pressed');
+})
